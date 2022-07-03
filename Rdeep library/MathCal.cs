915edf6d08c38f24c33071d161b19864a2303ec4
RigidWare <108437749+riyad03@users.noreply.github.com>
2022-07-03 20:53:15 +0100

    Add files via upload

---
+++ Rdeep library/MathCal.cs
@@ -0,0 +1,370 @@
+﻿using System;
+
+namespace RigidWare.Mathf
+{
+	public class MathCal
+	{
+        public static class Functions
+        {
+            public static float Sigmoide(float x)
+            {
+                float res = (float)(1 / (1 + Math.Exp(-(double)x)));
+                return res;
+            }
+
+            public static double Sigmoide(double x)
+            {
+                double res = (1 / (1 + Math.Exp(-(double)x)));
+                return res;
+            }
+            public static float Accuracy(double[] X, bool[] Y)
+            {
+                float TruePrediction = 0;
+                float AllPrediction = 0;
+
+                for (int i = 0; i < X.Length; i++)
+                {
+                    if (X[i] == 1 && Y[i] || X[i] != 1 && !Y[i])
+                    {
+                        TruePrediction += 1;
+                    }
+
+                    AllPrediction += 1;
+                }
+                return TruePrediction / AllPrediction;
+            }
+            public static float Accuracy(double[] X, double[] Y)
+            {
+                float TruePrediction = 0;
+                float AllPrediction = 0;
+
+                for (int i = 0; i < X.Length; i++)
+                {
+                    if (X[i] == Y[i])
+                    {
+                        TruePrediction += 1;
+                    }
+                    AllPrediction += 1;
+                }
+                return TruePrediction / AllPrediction;
+            }
+            public static double LogLoss(int a, int y, int i)
+            {
+                double ll = 0;
+                for (int x = 0; x < i; x++)
+                {
+                    ll += (y * Math.Log(a) + (1 - y) * Math.Log(1 - a));
+                }
+                return ll;
+            }
+        }
+        public static class Matrix
+        {
+            public static double[,] SetMatrix(double x, int m, int n)
+            {
+                double[,] Mat = new double[m, n];
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Mat[i, j] = x;
+                    }
+                }
+                return Mat;
+            }
+            public static double[,] Log(double[,] x)
+            {
+                double[,] res = new double[x.GetLength(0), x.GetLength(1)];
+
+                for (int i = 0; i < x.GetLength(0); i++)
+                {
+                    for (int j = 0; j < x.GetLength(1); j++)
+                    {
+                        res[i, j] = Math.Log(x[i, j]);
+                    }
+                }
+                return (res);
+
+            }
+
+            public static double[,] MatrixMultip(double[,] x, double[,] y)
+            {
+
+                double[,] result = new double[x.GetLength(0), y.GetLength(1)];
+
+                if (x.GetLength(1) == y.GetLength(0))
+                {
+                    for (int i = 0; i < x.GetLength(0); i++)
+                    {
+
+                        for (int c = 0; c < y.GetLength(1); c++)
+                        {
+                            result[i, c] = 0;
+                            for (int j = 0; j < x.GetLength(1); j++)
+                            {
+                                /*Console.Write(j + "  ");
+                                Console.WriteLine(y[j, c]);
+                                Console.Write(result[i, c] + " + ");*/
+
+                                result[i, c] += x[i, j] * y[j, c];
+                                //Console.WriteLine(x[i, j] + " * "+y[j, c] +") = "+ result[i, c]);
+
+
+
+                            }
+                        }
+                    }
+
+                    return result;
+                }
+                else
+                {
+
+
+                    throw new InvalidOperationException("the number of columns of the first matrix is different from the number of rows of the second matrix ");
+
+                }
+
+
+            }
+            public static double[,] MatrixMultipbyScalar(double y, double[,] x)
+            {
+                double[,] res = new double[x.GetLength(0), x.GetLength(1)];
+                for (int i = 0; i < x.GetLength(0); i++)
+                {
+                    for (int j = 0; j < x.GetLength(1); j++)
+                    {
+                        res[i, j] = x[i, j] * y;
+                    }
+                }
+                return res;
+            }
+            public static double[,] MatrixAdd(double[,] x, double[,] y)
+            {
+                double[,] sum = new double[x.GetLength(0), x.GetLength(1)];
+                if (x.GetLength(0) == y.GetLength(0) && x.GetLength(1) == y.GetLength(1))
+                {
+                    for (int i = 0; i < x.GetLength(0); i++)
+                        for (int j = 0; j < x.GetLength(1); j++)
+                            sum[i, j] = x[i, j] + y[i, j];
+
+                    return sum;
+                }
+                else
+                {
+                    throw new InvalidOperationException("The size of the first Matrix should be equal to size of second matrice");
+                }
+            }
+            public static double[,] MatrixAddToScalar(double y, double[,] x)
+            {
+                double[,] result = new double[x.GetLength(0), x.GetLength(1)];
+                for (int i = 0; i < x.GetLength(0); i++)
+                {
+                    for (int j = 0; j < x.GetLength(1); j++)
+                    {
+                        result[i, j] = x[i, j] + y;
+                    }
+                }
+                return result;
+            }
+            public static double[,] MatrixTrans(double[,] x)
+            {
+                double[,] result = new double[x.GetLength(1), x.GetLength(0)];
+
+                for (int i = 0; i < result.GetLength(0); i++)
+                {
+                    for (int j = 0; j < result.GetLength(1); j++)
+                    {
+                        result[i, j] = x[j, i];
+                    }
+                }
+                return result;
+            }
+            public static double[,] Concatenate(double[,] A, double[,] B, bool Vertical = true)
+            {
+                if (Vertical)
+                {
+                    if (A.GetLength(0) == B.GetLength(0))
+                    {
+                        double[,] res = new double[A.GetLength(0), A.GetLength(0) + B.GetLength(1) - 1];
+                        for (int i = 0; i < A.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < A.GetLength(1); j++)
+                            {
+                                res[i, j] = A[i, j];
+                            }
+                            for (int j = A.GetLength(1); j < A.GetLength(1) + B.GetLength(1); j++)
+                            {
+                                res[i, j] = B[i, j - A.GetLength(1)];
+                            }
+                        }
+                        return res;
+                    }
+                    else
+                    {
+
+                        throw new InvalidOperationException("the number of rows of the first Array must be equal to that of the second Array");
+
+                    }
+
+                }
+                else
+                {
+                    if (A.GetLength(1) == B.GetLength(1))
+                    {
+                        double[,] res = new double[A.GetLength(0) + B.GetLength(0), A.GetLength(1)];
+                        for (int i = 0; i < A.GetLength(1); i++)
+                        {
+                            for (int j = 0; j < A.GetLength(0); j++)
+                            {
+                                res[j, i] = A[j, i];
+                            }
+                            for (int j = A.GetLength(0); j < A.GetLength(0) + B.GetLength(0); j++)
+                            {
+
+                                res[j, i] = B[j - A.GetLength(0), i];
+                            }
+                        }
+                        return res;
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("the number of columns of the first Array must be equal to that of the second Array");
+                    }
+                }
+            }
+
+            public static double[,] Sigmoid(double[,] x)
+            {
+                double[,] res = new double[x.GetLength(0), x.GetLength(1)];
+
+                for (int i = 0; i < x.GetLength(0); i++)
+                {
+                    for (int j = 0; j < x.GetLength(1); j++)
+                    {
+
+                        res[i, j] = MathCal.Sigmoid(x[i, j]);
+                    }
+                }
+                return (res);
+            }
+
+        }
+        public static class Arr
+        {
+            public static double[] Setlinspace(double min, double max, int length)
+            {
+                length -= 1;
+                double[] res = new double[length];
+                double s = 0;
+                for (int i = 0; i < length; i++)
+                {
+                    s += (min + max) / (length);
+                    res[i] = s;
+                }
+                return res;
+            }
+            public static double[] Setarrange(double min, double max, double step)
+            {
+                int length = (int)(max / step) - 1;
+                double[] res = new double[length];
+                double s = 0;
+                for (int i = 0; i < length; i++)
+                {
+                    s += step;
+                    res[i] = s;
+                }
+                return res;
+            }
+            public static double[] Ravel(double[,] x)
+            {
+                double[] res = new double[x.Length];
+                int k = 0;
+                for (int i = 0; i < x.GetLength(0); i++)
+                {
+                    for (int j = 0; j < x.GetLength(1); j++)
+                    {
+                        res[k] = x[i, j];
+                        k++;
+                    }
+                }
+                return res;
+            }
+            public static bool[] Ravel(bool[,] x)
+            {
+                bool[] res = new bool[x.Length];
+                int k = 0;
+                for (int i = 0; i < x.GetLength(0); i++)
+                {
+                    for (int j = 0; j < x.GetLength(1); j++)
+                    {
+                        res[k] = x[i, j];
+                        k++;
+                    }
+                }
+                return res;
+            }
+            public static float[] Ravel(float[,] x)
+            {
+                float[] res = new float[x.Length];
+                int k = 0;
+                for (int i = 0; i < x.GetLength(0); i++)
+                {
+                    for (int j = 0; j < x.GetLength(1); j++)
+                    {
+                        res[k] = x[i, j];
+                        k++;
+                    }
+                }
+                return res;
+            }
+            public static int[] Ravel(int[,] x)
+            {
+                int[] res = new int[x.Length];
+                int k = 0;
+                for (int i = 0; i < x.GetLength(0); i++)
+                {
+                    for (int j = 0; j < x.GetLength(1); j++)
+                    {
+                        res[k] = x[i, j];
+                        k++;
+                    }
+                }
+                return res;
+            }
+
+
+        }
+        public static double Factorial(int n)
+        {
+            if (n > 1)
+            {
+                return n * Factorial(n - 1);
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        public static double Sigmoid(double x)
+        {
+            return 1 / (1 + Math.Exp(-x));
+        }
+        public static double RandomSND()
+        {
+            Random rand = new Random();
+            float y = rand.Next(10, 400);
+            y = y / 1000;
+            int x = rand.Next(0, 2);
+            if (x == 0)
+            {
+                return Math.Sqrt(-2 * Math.Log(Math.Sqrt(2 * 3.14) * y));
+            }
+            else
+            {
+                return -Math.Sqrt(-2 * Math.Log(Math.Sqrt(2 * 3.14) * y));
+            }
+        }
+        
+    }
+}
---
+++ Rdeep library/Rdeep.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.IO;
+using RigidWare.Mathf;
+
+namespace RigidWare.RDeep
+{
+	public class Rdeep
+	{
+        /// <summary>
+        /// Randomizes Weight values from the standard normalization distribution.
+        /// </summary>
+        /// <param name="X"></param>
+        /// <returns>Returns 2D Array Weight values from the standard normalization distribution.</returns>
+        public static double[,] InitialisationW(double[,] X)
+        {
+            Console.WriteLine("Initializing: 0%");
+            double[,] W = new double[X.GetLength(1), 1];
+            for (int i = 0; i < W.GetLength(0); i++)
+            {
+                double x = MathCal.RandomSND();
+
+                W[i, 0] = x;
+                Thread.Sleep(2);
+                Console.WriteLine(i * 100 / W.GetLength(0) + "%");
+            }
+
+
+            return W;
+        }
+        /// <summary>
+        /// Randomizes a Bias value from the standard normalization distribution.
+        /// </summary>
+        /// <param name="X"></param>
+        /// <returns>Returns a 2D arrays whose elements are equal to the Bias value.</returns>
+        public static double[,] InitialisationB(double[,] X)
+        {
+            double[,] B = new double[X.GetLength(1), 1];
+            B[0, 0] = MathCal.RandomSND();
+            for (int i = 0; i < B.GetLength(0); i++)
+            {
+                for (int j = 0; j < B.GetLength(1); j++)
+                {
+                    B[i, j] = B[0, 0];
+                }
+            }
+            return B;
+        }
+        /// <summary>
+        /// Reads Weight values that have been saved in FileName.
+        /// </summary>
+        /// <param name="FileName"></param>
+        /// <returns>Returns Weight values from FileName.</returns>
+        /// <exception cref="FileNotFoundException"></exception>
+        public static double[,] ReadW(string FileName)
+        {
+            int l,c;
+            if (File.Exists(FileName))
+            {
+                string[] r = File.ReadAllLines(FileName);
+                l = r.Length;
+                double[,] w = new double[l, r[0].Split(';').Length-1];
+
+                for (int i = 0; i < r.Length; i++) {
+                    r[i]=r[i].Remove(r[i].Length - 1);
+                    if (r[i].Contains(";"))
+                    {
+                        string[] s = r[i].Split(';');
+                        c = s.Length;
+
+                        for (int j = 0; j < c; j++)
+                        {
+                            if (s[j] != "")
+                            {
+                                w[i, j] = double.Parse(s[j]);
+                                Console.WriteLine("w "+w[i, j]);
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        w[i,0]= double.Parse(r[i]);
+                        
+                    }
+                    
+                }
+                
+                return w;
+            }
+            else
+            {
+                throw new FileNotFoundException(FileName+" doesn't Exist");
+            }
+            //double[,] w =
+        }
+
+        /// <summary>
+        /// Reads Bias values that have been saved in FileName.
+        /// </summary>
+        /// <param name="FileName"></param>
+        /// <returns>Returns Bias values from FileName.</returns>
+        /// <exception cref="FileNotFoundException"></exception>
+        public static double ReadB(string FileName)
+        {
+            return double.Parse(File.ReadAllText(FileName));
+        }
+        /// <summary>
+        /// Calculates the probabilty of prediction between 0 and 1.
+        /// </summary>
+        /// <param name="X"></param>
+        /// <param name="W"></param>
+        /// <param name="B"></param>
+        /// <returns>Returns an 2D array of probability values. </returns>
+        public static double[,] Model(double[,] X, double[,] W, double[,] B)
+        {
+
+            double[,] mult = MathCal.Matrix.MatrixMultip(X, W);
+           
+            double[,] z = MathCal.Matrix.MatrixAddToScalar(B[0, 0], mult);
+           
+            double[,] a = MathCal.Matrix.Sigmoid(z);
+
+
+            return a;
+        }
+
+        static double LogLoss(double[,] a, double[,] y)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    double e = 1e-15;
+                    Console.WriteLine(a[i, j]);
+                    sum += y[i, j] * Math.Log(a[i, j] + e) + (1 - y[i, j]) * Math.Log(1 - a[i, j]);
+
+                }
+            }
+
+            return -sum / y.GetLength(0);
+        }
+
+        static double[,] Gradients(double[,] y, double[,] a, double[,] x)
+        {
+
+            double[,] X = MathCal.Matrix.MatrixTrans(x);
+            double[,] A = MathCal.Matrix.MatrixMultipbyScalar(-1, a);
+            double[,] Y = MathCal.Matrix.MatrixAdd(y, A);
+            double[,] res = MathCal.Matrix.MatrixMultip(X, Y);
+           
+            return MathCal.Matrix.MatrixMultipbyScalar(-1f / y.GetLength(0), res);
+        }
+        static double[,] Gradients(double[,] y, double[,] a)
+        {
+
+
+            double[,] A = MathCal.Matrix.MatrixMultipbyScalar(-1, a);
+            double[,] AaddY = MathCal.Matrix.MatrixAdd(y, A);
+            double[,] res = MathCal.Matrix.MatrixMultipbyScalar(-1f / y.GetLength(0), AaddY);
+
+            return res;
+        }
+
+        static double[,] UpdateW(double[,] dw, double[,] w, double LearningRate)
+        {
+            double[,] dl = MathCal.Matrix.MatrixMultipbyScalar(-LearningRate, dw);
+            double[,] W = MathCal.Matrix.MatrixAdd(w, dl);
+            return W;
+        }
+        static double[,] UpdateB(double[,] db, double[,] b, double LearningRate)
+        {
+
+            double Bgradient = 0, bbrocast = 0;
+            //brocasting
+            for (int i = 0; i < db.GetLength(0); i++)
+            {
+                for (int j = 0; j < db.GetLength(1); j++)
+                {
+                    Bgradient += db[i, j];
+                }
+            }
+
+            bbrocast = b[0, 0];
+            double Scalarres = bbrocast + (-LearningRate * Bgradient);
+            double[,] res = new double[b.GetLength(0), b.GetLength(1)];
+            for (int i = 0; i < res.GetLength(0); i++)
+            {
+                for (int j = 0; j < res.GetLength(1); j++)
+                {
+                    res[i, j] = Scalarres;
+                    //Console.WriteLine("up"+res[i,j]);
+                }
+            }
+
+            return res;
+        }
+        /// <summary>
+        /// Calculates predictions in boolean form.
+        /// </summary>
+        /// <param name="X"></param>
+        /// <param name="W"></param>
+        /// <param name="b"></param>
+        /// <returns>Returns an 2D array of probability values in boolean form.</returns>
+        public static bool[,] Predict(double[,] X, double[,] W, double[,] b)
+        {
+            double[,] A = Model(X, W, b);
+            bool[,] result = new bool[A.GetLength(0), A.GetLength(1)];
+
+            for (int i = 0; i < result.GetLength(0); i++)
+            {
+                for (int j = 0; j < result.GetLength(1); j++)
+                {
+                    result[i, j] = (A[i, j] >= 0.5);
+                    
+
+                }
+                Console.WriteLine(i * 100 / result.GetLength(0) + "%");
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Trains an artificial neuron for a number of iterations depending on the learning rate.
+        /// </summary>
+        /// <param name="X"></param>
+        /// <param name="Y"></param>
+        /// <param name="Learning_rate"></param>
+        /// <param name="n_iter"></param>
+        /// <returns>Returns an array of logloss values. </returns>
+        public static double[] Artificial_neuron(double[,] X, double[,] Y, double Learning_rate = 0.1, int n_iter = 100)
+        {
+            List<double> L = new List<double>();
+
+            double[,] w = InitialisationW(X);
+
+            double[,] b = InitialisationB(X);
+            
+
+            for (int i = 0; i < n_iter; i++)
+            {
+                Console.WriteLine("A_N : "+i*100 / n_iter + "%");
+                double[,] A = Model(X, w, b);
+                L.Add(LogLoss(A, Y));
+                double[,] dw = Gradients(Y, A, X);
+                double[,] db = Gradients(Y, A);
+                w = UpdateW(dw, w, Learning_rate);
+                b = UpdateB(db, b, Learning_rate);
+                
+            }
+            File.WriteAllText(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\bias.txt",b[0,0]+"");
+            string currentData = "";
+            for (int i = 0; i < w.GetLength(0); i++)
+            {
+                
+            
+                for (int j=0;j< w.GetLength(1); j++)
+                {
+                    currentData += w[i, j]+";";
+                    
+                }
+                currentData += "\n";
+                
+            }
+            File.WriteAllText(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\weights.txt",
+                         currentData );
+            bool[,] pred = Predict(X, w, b);
+            
+            
+            //Console.WriteLine("percentage: " + MathCal.Functions.Accuracy(MathCal.Arr.Ravel(Y), MathCal.Arr.Ravel(pred)));
+            double[] res = new double[L.Count];
+            for (int i = 0; i < res.Length; i++)
+            {
+                res[i] = L[i];
+            }
+
+            return res;
+        }
+
+
+    
+    }
+}
